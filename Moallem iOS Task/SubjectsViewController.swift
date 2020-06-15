e09e68abf1d790a818d49a50cf960f501026a3e2
Abdalla Shawky <shaaawkyy@gmail.com>
2020-06-15 22:26:47 +0200

    Adding subjects ViewController

--- Moallem iOS Task/SubjectsViewController.swift
+++ Moallem iOS Task/SubjectsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SubjectsViewController.swift
 //  Moallem iOS Task
 //
 //  Created by Abdalla Shawky on 6/15/20.
@@ -8,15 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SubjectsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
-        
-        
     }
 
-
 }
-
